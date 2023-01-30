857abfc33af2fe16b183352ade2fdc50bebca8b1
Wendy <wendyvillazana@gmail.com>
2023-01-30 16:48:08 -0500

    iniciando proyecto

---
+++ src/main/java/com/example/api08_springboot_mongodb/Api08SpringBootMongoDb02Application.java
@@ -0,0 +1,13 @@
+package com.example.api08_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api08SpringBootMongoDb02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api08SpringBootMongoDb02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api08_springboot_mongodb/Api08SpringBootMongoDb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api08_springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api08SpringBootMongoDb02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
